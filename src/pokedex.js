5cd5a0895af47ded07121c05d66d1b5d547cc4c6
SiljeP <u2440913@rts-365.dk>
2023-10-24 08:34:30 +0200

    lav pokemon liste

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,23 +1,15 @@
 const UL = document.querySelector(".pokeList")
-// const URL = new URLSearchParams(window.location.search)
-// const OFFSET = parseInt(URL.get("offset") || 0)
-
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`)
-    .then(function (response) {
-        if (response.status !== 200)
-            throw new Error("error message")
-        return response.json()
-    })
-    .then(function (data) {
-        data.results.forEach(function (result) {
-            const LI = document.createElement("li")
-            LI.classList.add("pokeListItem")
-            LI.innerHTML = `${result.name}`
-            UL.append(LI)
-        })
-
-    })
-    .catch(function (error) {
-        console.log(error)
-
-    })
+    export default (function() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+            .then(res => res.json())
+            .then(data => {
+                data.results.forEach(pokemon => {
+                    const LI = document.createElement("li")
+                    LI.classList.add("pokeListItem")
+                    LI.innerText = pokemon.name
+                    document.body.append(p)
+                    UL.append(LI)
+                })
+            })
+    })()
+    
